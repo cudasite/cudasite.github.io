00ce717c6ca5a8c4b2aee8ace1d475d041dd6100
VitalikSt <vitalik.sts@gmail.com>
2016-08-02 12:00:19 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/************fancybox***************/
+$(document).ready(function() {
+		$(".fancybox").fancybox();
+	});
+/***************//*****************/
+
+/************menu_h***************/
+$(document).ready(function(){
+	$('.menu_h').click(function() {
+		$('nav ul').slideToggle('slow');
+	});
+});
+/***************//*****************/
