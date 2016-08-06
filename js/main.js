989c9075b159bf496a2b5b7971f04ed82e7174ae
VitalikSt <vitalik.sts@gmail.com>
2016-08-07 00:38:22 +0300

    initiall commit

--- js/main.js
+++ js/main.js
@@ -9,5 +9,17 @@ $(document).ready(function(){
 	$('.menu_h').click(function() {
 		$('nav ul').slideToggle('slow');
 	});
+  $('nav ul li').click(function(){
+    $('nav ul').slideUp('slow')
+  });
 });
 /***************//*****************/
+  $(document).ready(function(){
+    $("nav ul li").on("click","a", function (event) {
+              event.preventDefault();
+     var id  = $(this).attr('href'),
+     top = $(id).offset().top;         
+    $('body,html').animate({scrollTop: top}, 1000);
+    });
+    })
+/*******************//****************/
